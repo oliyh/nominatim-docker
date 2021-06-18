a83bc5c3ace38bc927f8c4340645f5a33cee7f5c
Oliver Hine <github@oliy.co.uk>
2021-06-18 16:26:52 +0100

    prompt for import

--- 3.7/start.sh
+++ 3.7/start.sh
@@ -1,33 +1,23 @@
 #!/bin/bash -ex
 
-stopServices() {
-  service apache2 stop
-  service postgresql stop
-}
-trap stopServices TERM
-
-/app/config.sh
-
-if id nominatim >/dev/null 2>&1; then
-  echo "user nominatim already exists"
-else
-  useradd -m -p ${NOMINATIM_PASSWORD} nominatim
-fi
-
 IMPORT_FINISHED=/var/lib/postgresql/12/main/import-finished
 
 if [ ! -f ${IMPORT_FINISHED} ]; then
-  /app/init.sh
-  touch ${IMPORT_FINISHED}
+  echo "Import has not been completed, would you like to run it?"
+  select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) /app/start2.sh; break;;
+        No ) exit;;
+    esac
+  done
 else
   chown -R nominatim:nominatim ${PROJECT_DIR}
+  cd ${PROJECT_DIR} && nominatim refresh --website
+
+  service postgresql start
+  service apache2 start
+
+  # fork a process and wait for it
+  tail -f /var/log/postgresql/postgresql-12-main.log &
+  wait
 fi
-
-cd ${PROJECT_DIR} && nominatim refresh --website
-
-service postgresql start
-service apache2 start
-
-# fork a process and wait for it
-tail -f /var/log/postgresql/postgresql-12-main.log &
-wait
